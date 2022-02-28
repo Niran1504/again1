3a4b6420f16a644f006ab7cd0a53c062b64c4b5e
niran <niranawesome15@gmail.com>
2022-02-28 12:47:05 +0530

    kiai

---
+++ peacemaker.py
@@ -0,0 +1,4 @@
+a = {"actor" : "john cena", "villain" : "goff"}
+b = {"sidekick" : "vigilante", "comedy villain" : "judomaster"}
+c = {"show" : "peacemaker", "year" : 2022}
+d = {"director" : "james gunn", "dop" : "cody"}
